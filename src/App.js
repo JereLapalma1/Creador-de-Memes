f364357b57e15caa1c274fe890a7f60f86f23467
Jeremias <87451742+JereLapalma1@users.noreply.github.com>
2022-02-23 18:28:23 -0300

    Creador de memes v2.0 mejorado con bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import $ from 'jquery';
-import {size , size1 ,size2} from 'jquery';
 import html2canvas from 'html2canvas';
+import { Button, Form, FloatingLabel, Image, Container } from 'react-bootstrap';
 
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'boxicons'
 
 
 function App() {
@@ -37,154 +39,106 @@ function App() {
       link.click();
     });
   }
-
-
   // ----------------CAMBIO DE MARGEN-----------
-  /*let size;
-  let size1;
-*/
-  function tamañoMargen() {
-    size = $(".texto1").css("margin-top");
-    size = parseInt(size, 10);
-    $(".tamanioActual").text(size);
-  }
+  $(".disminuir").on("click", function () {$(".texto1").css("margin-top", "-=1");});
 
-  // Obtengo el tamaño de letra inicial de 16px 
-  tamañoMargen();
+  $(".aumentar").on("click", function () {$(".texto1").css("margin-top", "+=1");});
+  $(".disminuir2").on("click", function () {$(".texto2").css("margin-top", "-=1");});
 
-  // Función para disminuir el tamaño del texto (fuente) 
-  $(".disminuir").on("click", function () {
-    if ((size - 2) >= 0) {
-      $(".texto1").css("margin-top", "-=2");
-      $(".tamanioActual").text(size -= 1);
-    }
-  });
-
-  // Función para aumentar el tamaño del texto (fuente) 
-  $(".aumentar").on("click", function () {
-    if ((size + 2) <= 800) {
-      $(".texto1").css("margin-top", "+=2");
-      $(".tamanioActual").text(size += 1);
-    }
-  });
-  function tamañoMargen1() {
-    size1 = $(".texto2").css("margin-top");
-    size1 = parseInt(size1, 10);
-    $(".tamanioActual2").text(size1);
-  }
-
-  // Obtengo el tamaño de letra inicial de 16px 
-  tamañoMargen1();
-
-  // Función para disminuir el tamaño del texto (fuente) 
-  $(".disminuir2").on("click", function () {
-    if ((size1 - 2) >= 0) {
-      $(".texto2").css("margin-top", "-=2");
-      $(".tamanioActual2").text(size1 -= 1);
-    }
-  });
-
-  // Función para aumentar el tamaño del texto (fuente) 
-  $(".aumentar2").on("click", function () {
-    if ((size1 + 2) <= 800) {
-      $(".texto2").css("margin-top", "+=2");
-      $(".tamanioActual2").text(size1 += 1);
-    }
-  });
+  $(".aumentar2").on("click", function () {$(".texto2").css("margin-top", "+=1");});
   //----------------FIN  CAMBIO DE MARGEN -----------
 
   // ----------------- CAMBIAR COLOR DE TEXTO
-  $(".cambiar").on('click', function () {
-    $(".texto1").css("color", "black");
-    $(".texto2").css("color", "black");
-    $(".texto1").css("text-shadow", "2px 2px 4px white");
-    $(".texto2").css("text-shadow", "2px 2px 4px white");
-  })
+  $(".cambiar").on('click', function () {$(".texto1").css("color", "black");$(".texto2").css("color", "black");$(".texto1").css("text-shadow", "2px 2px 4px white");$(".texto2").css("text-shadow", "2px 2px 4px white");})
 
-  $(".restablecer").on("click", function () {
-    $(".texto1").css("color", "white");
-    $(".texto2").css("color", "white");
-    $(".texto1").css("text-shadow", "2px 2px 4px black");
-    $(".texto2").css("text-shadow", "2px 2px 4px black");
-  })
+  $(".restablecer").on("click", function () {$(".texto1").css("color", "white");$(".texto2").css("color", "white");$(".texto1").css("text-shadow", "2px 2px 4px black");$(".texto2").css("text-shadow", "2px 2px 4px black");})
   //--------------------------- fin cambiar color------------
 
 
 
   //---------------AUMENTAR FUENTE------------------------
+  $(".disminuir-fuente").on("click", function () {$(".texto1").css("font-size", "-=1");});
 
-  //let size2;
-// Defino un tamaño de letra inicial de 16px
-function tamañoLetra() {
-  size2 = $(".texto1" ).css("font-size");
-  size2 = parseInt(size2, 10);
-  $( ".tamañoactual" ).text(size2);
-}
+  $(".aumentar-fuente").on("click", function () {$(".texto1").css("font-size", "+=1");});
 
-tamañoLetra();
+  $(".disminuir-fuente2").on("click", function () {$(".texto2").css("font-size", "-=1");});
 
-$(".disminuir-fuente").on("click", function() {
-  if ((size2- 2) >= 0) {
-    $(".texto1").css("font-size", "-=2");
-    $(".tamañoactual").text(size2 -= 1);
-  }
-});
-
-$(".aumentar-fuente").on("click", function() { 
-  if ((size2 + 2) <= 52) {
-    $(".texto1").css("font-size", "+=2");
-    $(".tamañoactual").text(size2 += 1);
-  }
-});
-
-
- 
-
-
+  $(".aumentar-fuente2").on("click", function () {$(".texto2").css("font-size", "+=1"); });
   //-----------------------FIN AUMENTAR FUENTE--------------
-
-
-  /*onchange  recibe una funcion o codigo js  , 
-  que permite  ejecutar algun tipo de accion al componente
-  */
+/*onchange  recibe una funcion o codigo js  , 
+que permite  ejecutar algun tipo de accion al componente
+*/
   return (
-    <div className="App">
-      <select onChange={imagenCambio}>
-        <option value="3" key="fry">Fry Enojado</option>
-        <option value="1" key="dexter">Dexter</option>
-        <option value="2" key="mordekay">Mordekay</option>
-        <option value="4" key="dinopensante">Dino Pensante</option>
-      </select><br />
+
+    <Container fluid className='pe-2 ps-2 pt-2 pb-2 shadow-lg'>
+
+      <div className="App bg-dark pt-4">
+        <div className='container mt-4 p-0'>
+          <Form.Select size="sm" className='bg-light' onChange={imagenCambio}>
+            <option value="3" key="fry">Fry Enojado</option>
+            <option value="1" key="dexter">Dexter</option>
+            <option value="2" key="mordekay">Mordekay</option>
+            <option value="4" key="dinopensante">Dino Pensante</option>
+          </Form.Select><br />
 
 
-      <input onChange={linea1Cambio} type="text" placeholder='linea 1' /><br />
-      <input onChange={linea2Cambio} type="text" placeholder='linea 2' /><br />
 
-      <div class="btn-toolbar" role="toolbar" aria-label="Botones">
-        <button type="button" className="disminuir "> Mover arriba texto 1</button>
-        <button type="button" className="aumentar ">Mover abajo texto 1</button> <br />
-        <button type="button" className="disminuir2 "> Mover arriba texto 2</button>
-        <button type="button" className="aumentar2 ">Mover abajo texto 2</button> <br />
-        <button type='button' className='cambiar'   >Cambiar color</button>
-        <button type='button' className='restablecer'   >restablecer color</button>
-        <p> Margen1: <span className='tamanioActual'></span> <span className='tamanioActual2'></span></p>
+          <FloatingLabel controlId="floatingTextarea" label="Linea 1" className="linea1 mb-3">
+            <Form.Control as="textarea" onChange={linea1Cambio} placeholder="linea 1" />
+            <div className='row' >
+              <div className='col-lg-2  col-md-2 col-sm-2 p-0'>
+                <Button type="button" variant="danger" className="disminuir mt-2 me-2 btn-sm "  ><box-icon name='up-arrow-alt'></box-icon></Button>
+                <Button type="button" variant="danger" className="aumentar mt-2 btn-sm "><box-icon name='down-arrow-alt' ></box-icon></Button> <br />
+              </div>
+              <div className='col-lg-2  col-md-2 col-sm-2'>
+                <Button type='button' variant="danger" className='aumentar-fuente btn-sm mt-2 ps-1 text-dark fw-bold'   ><box-icon name='font-size'></box-icon>+</Button>
+                <Button type="button" variant="danger" className="disminuir-fuente btn-sm mt-2 text-dark fw-bold"><box-icon name='font-size'></box-icon>-</Button>
+              </div>
+              <div className='col-lg-2  col-md-2 col-sm-2'>
+               <Button type='button' variant="danger" className='cambiar btn-sm mt-2 ps-1 ms-3 me-4'   ><box-icon name='font-color'></box-icon></Button>
+               <Button type='button' variant="danger" className='restablecer btn-sm mt-2 '   ><box-icon name='font-color'></box-icon></Button>
+              </div>
+            </div>
+
+          </FloatingLabel>
+
+          <FloatingLabel controlId="floatingTextarea" label="Linea 2" className="mb-3">
+            <Form.Control as="textarea" onChange={linea2Cambio} placeholder="linea 2" />
+
+            <div className='row' >
+              <div className='col-lg-2  col-md-2 col-sm-2'>
+                <Button type="button" variant="danger" className="disminuir2 mt-2 me-2 btn-sm " ><box-icon name='up-arrow-alt'></box-icon></Button>
+                <Button type="button" variant="danger" className="aumentar2 mt-2 btn-sm "><box-icon name='down-arrow-alt' ></box-icon></Button> <br />
+              </div>
+              <div className='col-lg-2  col-md-2 col-sm-2'>
+                <Button type='button' variant="danger" className='aumentar-fuente2 btn-sm mt-2 ps-1 ms-3 me-4 text-dark fw-bold'   ><box-icon name='font-size'></box-icon>+</Button>
+                <Button type="button" variant="danger" className="disminuir-fuente2 btn-sm mt-2 text-dark fw-bold"><box-icon name='font-size'></box-icon>-</Button>
+
+              </div>
+              <div className='col-lg-2  col-md-2 col-sm-2'>
+               <Button type='button' variant="danger" className='cambiar btn-sm mt-2 ps-1 ms-3 me-4'   ><box-icon name='font-color'></box-icon></Button>
+               <Button type='button' variant="danger" className='restablecer btn-sm mt-2 '   ><box-icon name='font-color'></box-icon></Button>
+              </div>
+            </div>
+
+          </FloatingLabel>
+
+        </div>
 
 
-        <span className='tamañoactual'></span> <br/>
-      <button type='button' className='aumentar-fuente'   >aumentar fuente</button>
-      <button type="button" className="disminuir-fuente "> disminuir fuente</button>
+        <Button variant="success" onClick={exportarArchivo} className="mt-2 ">Exportar</Button>
+
+        <div className='meme' id='meme'>
+          <span className='texto1'>{linea1}<br /> </span> <br />
+          <span className='texto2'>{linea2} </span>
+          <Image className='thumbnail border border-2  mb-3 ' src={"/img/" + image + ".jpg"} />
+        </div>
+
+
+
       </div>
-      <button onClick={exportarArchivo}>Exportar</button>
+    </Container>
 
-      <div className='meme' id='meme'>
-        <span className='texto1'>{linea1}<br /> </span> <br />
-        <span className='texto2'>{linea2} </span>
-        <img src={"/img/" + image + ".jpg"} />
-      </div>
-
-
-      
-    </div>
   );
 }
 
